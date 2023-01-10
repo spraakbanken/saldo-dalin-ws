214b3938e5f6853defb3e9ad87e71676cf390d0e
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-01-10 21:16:30 +0100

    refactor: introduce LookupService

--- sblex/application/services/lookup.py
+++ sblex/application/services/lookup.py
@@ -1,30 +1,39 @@
+import abc
 
-def lookup_lid(lid):
-    from handler import is_lemma, is_lexeme
-    if (is_lemma(lid)):
-        from lemma import function
-    elif (is_lexeme(lid)):
-        from lexeme import function
-    else:
-        function = None
-        ret = []
-    if function is not None:
-        (result, result_code) = function('json', lid)
-        ret = cjson.decode(utf8.d(result))
-    return ret
+from sblex.predicates import is_lemma, is_lexeme
 
-def md1(sense_id):
-    xs = []
-    sib = []
-    res = lookup_lid(sense_id)
-    if res == []:
-        return []
-    if res['fm'] != 'PRIM..1':
-        sib = lookup_lid(utf8.e(res['fm']))['mf']
-        xs = [res['fm']]
-    md1 = xs + sib + res['mf']
-#    wf = []
-#    for s in md1:
-#        wf = wf + wordforms(utf8.e(s))
-    return list(set(md1))
+
+class LookupService(abc.ABC):
+    @abc.abstractmethod
+    def lookup_lemma(self, lid):
+        ...
+
+    @abc.abstractmethod
+    def lookup_lexeme(self, lid):
+        ...
+
+
+    def lookup_lid(self, lid):
+        if is_lemma(lid):
+            ret = self.lookup_lemma(lid)
+        elif is_lexeme(lid):
+            ret = self.lookup_lexeme(lid)
+        else:
+            ret = []
+        return ret
+
+    def md1(self, sense_id):
+        xs = []
+        sib = []
+        res = self.lookup_lid(sense_id)
+        if res == []:
+            return []
+        if res['fm'] != 'PRIM..1':
+            sib = self.lookup_lid(res['fm'])['mf']
+            xs = [res['fm']]
+        md1 = xs + sib + res['mf']
+    #    wf = []
+    #    for s in md1:
+    #        wf = wf + wordforms(utf8.e(s))
+        return list(set(md1))
 
